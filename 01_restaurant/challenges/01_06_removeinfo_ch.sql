bb716e5362ea2f4bcccec12c4f7a375c592b5587
Martín Chiri <43660072+MartinChiri@users.noreply.github.com>
2022-11-22 19:12:10 +0000

    Restaurant Challenges 4 to 9

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,21 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+DELETE FROM Reservations
+WHERE Date >= '2022-07-24' 
+AND CustomerID = (
+    SELECT CustomerID
+    FROM Customers
+    WHERE FirstName = 'Norby');
+-- OR
+
+UPDATE Reservations
+SET Date = NULL
+WHERE Date >=  '2022-07-24'
+AND  CustomerID = (
+    SELECT CustomerID
+    FROM Customers
+    WHERE FirstName = 'Norby');
+
+  SELECT * FROM Reservations WHERE Date >= '2022-07-24';
--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,13 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT *
+FROM Reservations
+WHERE Date BETWEEN '2022-06-14' AND '2022-06-15'
+AND CustomerID IN (
+    SELECT CustomerID
+    FROM Customers
+    WHERE FirstName LIKE 'St%n' 
+    OR LastName LIKE 'St%n'
+);
