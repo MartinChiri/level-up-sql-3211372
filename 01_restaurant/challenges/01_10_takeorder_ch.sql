cf23e159b9f7360992c59af99d09779408fd8381
Martín Chiri <43660072+MartinChiri@users.noreply.github.com>
2022-11-22 19:57:08 +0000

    first attempt rest. 10 challe

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,46 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+INSERT INTO Orders (CustomerID, OrderDate)
+VALUES (
+    (SELECT CustomerID
+      FROM Customers
+      WHERE FirstName = 'Loretta'
+      AND Address LIKE '6939 Elka%'),
+    '2022-09-20 14:00:00'
+);
+
+INSERT INTO OrdersDishes (OrderID, DishID)
+SELECT o.OrderID, d.DishID
+FROM 
+    (SELECT OrderID
+      FROM Orders
+      WHERE CustomerID = (
+        SELECT CustomerID
+        FROM Customers
+        WHERE FirstName = 'Loretta'
+        AND Address LIKE '6939 Elka%')
+      AND OrderDate = '2022-09-20 14:00:00') AS o
+CROSS JOIN
+    (SELECT DishID
+      FROM Dishes
+      WHERE Name IN ('House Salad', 'Mini Cheeseburgers',
+        'Tropical Blue Smoothie')
+    ) AS d
+
+Select o.OrderID, SUM(Price) as Total
+FROM Orders AS o
+INNER JOIN OrdersDishes AS od
+ON o.OrderID = od.OrderID
+INNER JOIN Dishes AS d
+ON od.DishID = d.DishID
+WHERE o.OrderID = (
+    SELECT OrderID
+    FROM Orders
+    WHERE CustomerID = (
+      SELECT CustomerID
+      FROM Customers
+      WHERE FirstName = 'Loretta'
+      AND Address LIKE '6939 Elka%')
+    AND OrderDate = '2022-09-20 14:00:00')
+GROUP BY o.OrderID;
